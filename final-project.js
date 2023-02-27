efb3298433f89df8a3c1a64ae87f2b7534fb2b11
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-02-26 22:27:23 -0800

    Replicate Shadertoy's default project

--- final-project.js
+++ final-project.js
@@ -10,12 +10,14 @@ export class Final_Project extends Scene {
 
         this.shapes = {
             torus: new defs.Torus(15, 15),
+            cube: new defs.Cube(),
         };
 
         this.materials = {
             test: new Material(new defs.Phong_Shader(),
-                {ambient: .4, diffusivity: .6, color: hex_color("#ff0000")}),
+                {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
             refract: new Material(new Refract_Shader()),
+            rainbow: new Material(new Rainbow_Shader()),
         };
     }
 
@@ -41,8 +43,13 @@ export class Final_Project extends Scene {
     display(context, program_state) {
         this.setup(context, program_state);
 
-        const light_position = vec4(0, 5, 5, 1);
-        program_state.lights = [new Light(light_position, color(1, 1, 1, 1), 1000)];
+        this.shapes.cube.draw(
+            context,
+            program_state,
+            Mat4.identity().times(Mat4.scale(4, 3, 1))
+                           .times(Mat4.translation(0, 0, -5)),
+            this.materials.rainbow
+        );
 
         this.shapes.torus.draw(
             context,
@@ -54,8 +61,8 @@ export class Final_Project extends Scene {
 }
 
 class Refract_Shader extends Shader {
-    update_GPU(context, gpu_addresses, graphics_state, model_transform, material) {
-        const [P, C, M] = [graphics_state.projection_transform, graphics_state.camera_inverse, model_transform],
+    update_GPU(context, gpu_addresses, program_state, model_transform, material) {
+        const [P, C, M] = [program_state.projection_transform, program_state.camera_inverse, model_transform],
             PCM = P.times(C).times(M);
         context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
         context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
@@ -87,6 +94,52 @@ class Refract_Shader extends Shader {
         return this.shared_glsl_code() + `
             void main() {
                 gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        `
+    }
+}
+
+class Rainbow_Shader extends Shader {
+    update_GPU(context, gpu_addresses, program_state, model_transform, material) {
+        const [P, C, M] = [program_state.projection_transform, program_state.camera_inverse, model_transform],
+            PCM = P.times(C).times(M);
+        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
+        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
+            Matrix.flatten_2D_to_1D(PCM.transposed()));
+        context.uniform1f(gpu_addresses.iTime, program_state.animation_time / 1000);
+    }
+
+    shared_glsl_code() {
+        return `
+            precision mediump float;
+
+            varying vec3 fragCoord;
+        `;
+    }
+
+    vertex_glsl_code() {
+        return this.shared_glsl_code() + `
+            attribute vec3 position, normal;
+            uniform mat4 model_transform;
+            uniform mat4 projection_camera_model_transform;
+
+            void main() {
+                gl_Position = projection_camera_model_transform * vec4( position, 1.0 );
+                fragCoord = position;
+            }
+        `;
+    }
+
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            uniform float iTime;
+
+            void main() {
+                vec3 uv = fragCoord;
+
+                vec3 col = 0.5 + 0.5*cos(iTime+uv+vec3(0,2,4));
+
+                gl_FragColor = vec4(col, 1.0);
             }
         `
     }
