702ffe38163a230e6c523c84da68d092509d4e92
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-02-27 01:29:09 -0800

    stress shader v1

--- src/final-project.js
+++ src/final-project.js
@@ -1,6 +1,5 @@
 import {defs, tiny} from '../examples/common.js';
-import { Refract_Shader } from './refract-shader.js';
-import { Rainbow_Shader } from './rainbow-shader.js';
+import { Stress_Shader, Force } from './stress-shader.js';
 
 const {
     Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
@@ -13,13 +12,15 @@ export class Final_Project extends Scene {
         this.shapes = {
             torus: new defs.Torus(15, 15),
             cube: new defs.Cube(),
+            s4: new defs.Subdivision_Sphere(4),
+            cylinder: new defs.Capped_Cylinder(50, 15),
         };
 
         this.materials = {
             test: new Material(new defs.Phong_Shader(),
                 {ambient: .4, diffusivity: .6, color: hex_color("#ffffff")}),
-            refract: new Material(new Refract_Shader()),
-            rainbow: new Material(new Rainbow_Shader()),
+            stress: new Material(new Stress_Shader(),
+                {forces: [], min_color: color(0, 0, 1, 1), max_color: color(1, 0, 0, 1), min_force: -10, max_force: 10}),
         };
     }
 
@@ -28,7 +29,7 @@ export class Final_Project extends Scene {
         if (!context.scratchpad.controls) {
             this.children.push(context.scratchpad.controls = new defs.Movement_Controls());
             // Define the global camera and projection matrices, which are stored in program_state.
-            program_state.set_camera(Mat4.translation(0, 0, -5));
+            program_state.set_camera(Mat4.translation(0, 0, -30));
         }
         program_state.projection_transform = Mat4.perspective(
             Math.PI / 4, context.width / context.height, 1, 100);
@@ -45,19 +46,22 @@ export class Final_Project extends Scene {
     display(context, program_state) {
         this.setup(context, program_state);
 
-        this.shapes.cube.draw(
-            context,
-            program_state,
-            Mat4.identity().times(Mat4.scale(4, 3, 1))
-                           .times(Mat4.translation(0, 0, -5)),
-            this.materials.rainbow
-        );
+        const t = program_state.animation_time / 1000;
+        const period = 5;
+        const force = 0.5 + 0.5*Math.cos(2*Math.PI*t/period);
 
-        this.shapes.torus.draw(
+        const forces = [
+            new Force(vec3(0, 1, 0), 10 * force, vec3(-10, 0, 0)),
+            new Force(vec3(0, -1, 0), 20 * force, vec3(0, 0, 0)),
+            new Force(vec3(0, 1, 0), 10 * force, vec3(10, 0, 0)),
+        ];
+
+        this.shapes.cylinder.draw(
             context,
             program_state,
-            Mat4.identity(),
-            this.materials.refract,
+            Mat4.identity().times(Mat4.rotation(Math.PI/2, 0, 1, 0))
+                           .times(Mat4.scale(1, 1, 20)),
+            this.materials.stress.override({forces: forces})
         );
     }
 }
---
+++ src/stress-shader.js
@@ -0,0 +1,100 @@
+import { defs, tiny } from '../examples/common.js';
+
+const {
+    Vector, Vector3, vec, vec3, vec4, color, hex_color, Shader, Matrix, Mat4, Light, Shape, Material, Scene,
+} = tiny;
+
+export class Force {
+    constructor(direction, magnitude = 1, position = vec3(0, 0, 0)) {
+        this.direction = direction;
+        this.magnitude = magnitude;
+        this.position = position;
+    }
+
+    get normalized_direction() {
+        return this.direction.normalized();
+    }
+}
+
+export class Stress_Shader extends Shader {
+    constructor(num_forces = 100) {
+        super();
+        this.num_forces = num_forces;
+    }
+
+    update_GPU(context, gpu_addresses, program_state, model_transform, material) {
+        const [P, C, M] = [program_state.projection_transform, program_state.camera_inverse, model_transform],
+            PCM = P.times(C).times(M);
+        context.uniformMatrix4fv(gpu_addresses.model_transform, false, Matrix.flatten_2D_to_1D(model_transform.transposed()));
+        context.uniformMatrix4fv(gpu_addresses.projection_camera_model_transform, false,
+            Matrix.flatten_2D_to_1D(PCM.transposed()));
+
+        context.uniform4fv(gpu_addresses.min_color, material.min_color);
+        context.uniform4fv(gpu_addresses.max_color, material.max_color);
+        context.uniform1f(gpu_addresses.min_force, material.min_force);
+        context.uniform1f(gpu_addresses.max_force, material.max_force);
+
+        const flattened_forces = [];
+        const flattened_force_positions = [];
+        for (const force of material.forces) {
+            const force_components =  force.normalized_direction.times(force.magnitude);
+            flattened_forces.push(...force_components);
+            flattened_force_positions.push(...force.position);
+        }
+        context.uniform3fv(gpu_addresses.forces, flattened_forces);
+        context.uniform3fv(gpu_addresses.force_positions, flattened_force_positions);
+    }
+
+    shared_glsl_code() {
+        return `
+            precision mediump float;
+
+            const int N_FORCES = ` + this.num_forces + `;
+
+            uniform vec3 forces[N_FORCES];
+            uniform vec3 force_positions[N_FORCES];
+            uniform float min_force;
+            uniform float max_force;
+
+            varying float shear_force;
+        `;
+    }
+
+    vertex_glsl_code() {
+        return this.shared_glsl_code() + `
+            uniform mat4 model_transform;
+            uniform mat4 projection_camera_model_transform;
+
+            attribute vec3 position;
+
+            void main() {
+                vec4 homogenous_position = vec4( position, 1.0 );
+
+                gl_Position = projection_camera_model_transform * homogenous_position;
+                vec4 world_space_coord = model_transform * homogenous_position;
+
+                shear_force = 0.0;
+                for (int i = 0; i < N_FORCES; i++) {
+                    if (force_positions[i].x < world_space_coord.x)
+                        shear_force += forces[i].y;
+                }
+            }
+        `;
+    }
+
+    fragment_glsl_code() {
+        return this.shared_glsl_code() + `
+            uniform vec4 min_color;
+            uniform vec4 max_color;
+
+            void main() {
+                float clamped_shear = clamp( shear_force, min_force, max_force );
+
+                float range = max_force - min_force;
+                float a = (clamped_shear - min_force) / range;
+
+                gl_FragColor = mix( min_color, max_color, a );
+            }
+        `;
+    }
+}
